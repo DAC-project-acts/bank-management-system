854ae67fc5c0bdb301b4b45bd3e0b5b9365165bd
chetan7077 <chetan.t2001@gmail.com>
2025-02-12 18:38:36 +0530

    updated AccountOperation entities class

--- digital.banking/src/main/java/com/bank/entities/AccountOperation.java
+++ digital.banking/src/main/java/com/bank/entities/AccountOperation.java
@@ -1,29 +1,32 @@
 package com.bank.entities;
 
-import java.util.Date;
-
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 import com.bank.enums.OperationType;
 
-public class AccountOperation 
-{
-	 @Id 
-	 @GeneratedValue(strategy = GenerationType.IDENTITY)
-	 private Long id; 
-	 private Date operationDate;
-	 private double amount;
-	 private OperationType type;
-	 private String description;
+import javax.persistence.*;
+import java.util.Date;
 
-	 @ManyToOne
-	 @OnDelete(action = OnDeleteAction.CASCADE)
-	  private BankAccount bankAccount;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class AccountOperation {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ;
+    private Date operationDate;
+    private double amount;
+    private OperationType type;
+    private String description;
+
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private BankAccount bankAccount;
+
 
 }
