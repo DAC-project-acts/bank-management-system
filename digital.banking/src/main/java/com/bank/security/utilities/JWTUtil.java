2459d6d23a0142a3f8b5538c0896a11fa2f6e02c
Vishal1169 <vishalrathod1169@gmail.com>
2025-02-13 19:20:15 +0530

    Added JWT utility class for managing authentication constants including secret key, token expiration times, and authorization header format.

--- digital.banking/src/main/java/com/bank/security/utilities/JWTUtil.java
+++ digital.banking/src/main/java/com/bank/security/utilities/JWTUtil.java
@@ -1,9 +1,9 @@
 package com.bank.security.utilities;
 
 public class JWTUtil {
-    public static final String SECRET = "shamgirhe";
+    public static final String SECRET = "cdac-Acts";
     public static final String AUTH_HEADER = "Authorization";
     public static final long EXPIRE_ACCESS_TOKEN = 80 * 80 * 1000;
     public static final long EXPIRE_REFRESH_TOKEN = 500 * 80 * 1000;
-    public static final String PREFIX = "Bearer ";
+    public static final String PREFIX = "Bearer";
 }
